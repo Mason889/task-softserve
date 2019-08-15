6a64bba02a2736477c646481e13546ca9bab2026
Konstantin <kostya.zaigraev@gmail.com>
2019-08-15 13:57:18 -0400

    Change bash-script (add comments for code + variables)

--- gcp/files/deploy.sh
+++ gcp/files/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+###	Initializying of variables	 ###
+name_of_project=devops-practice-247821								# name of your GCP project
+key_file=devops-practice-247821-005171c93906.json						# credential of project's service-account
+gcp_service_account=testing-terraform@devops-practice-247821.iam.gserviceaccount.com		# name of project's service account 
+###		Commands		 ###
 sudo apt-get update
 sudo apt-get install init -y nginx
-gcloud auth activate-service-account testing-terraform@devops-practice-247821.iam.gserviceaccount.com --key-file=devops-practice-247821-005171c93906.json --project=devops-practice-247821
+sudo gcloud auth activate-service-account $gcp_service_account --key-file=$key_file --project=$name_of_project
 sudo gsutil cp gs://testing-nginx/hello.html /var/www/html 
 sudo rm -rf /etc/nginx/sites-enabled/*
 sudo gsutil cp gs://testing-nginx/nginx /etc/nginx/sites-enabled/
